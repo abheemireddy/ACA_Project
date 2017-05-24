e736417591d2d55496a98d57a8d56b3988f45cd7
chad <ccrowe@unomaha.edu>
2017-05-24 11:33:42 -0500

    trying to get a hashmap implementation.

---
+++ HashMap.h
@@ -0,0 +1,10 @@
+//
+// Created by chad on 5/24/17.
+//
+//basic outline for this hashmap from http://www.kaushikbaruah.com/posts/data-structure-in-c-hashmap/
+
+#ifndef ACA_PROJECT_HASHMAP_H
+#define ACA_PROJECT_HASHMAP_H
+
+
+#endif //ACA_PROJECT_HASHMAP_H
