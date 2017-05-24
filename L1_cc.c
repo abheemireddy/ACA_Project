eeb3a296cc8b02fc0d7f89e55500a0dccc1077e4
chad <ccrowe@unomaha.edu>
2017-05-24 09:09:10 -0500

    Added L1_cc header with basic block structure

--- L1_cc.c
+++ L1_cc.c
@@ -1,2 +1,1 @@
 //This is the L1 cache controller
-//I have my CLion up and running, going to try and push to github
---
+++ L1_cc.h
@@ -0,0 +1,19 @@
+//
+// Created by chad on 5/24/17.
+//
+
+#ifndef ACA_PROJECT2_L1_CC_H
+#define ACA_PROJECT2_L1_CC_H
+
+#include <stdbool.h>
+
+struct Block{
+    bool validBit;
+    bool dirtyBit;
+    char* tag;
+    int blockSize;
+};
+
+
+
+#endif //ACA_PROJECT2_L1_CC_H
