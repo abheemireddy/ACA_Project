7462f0f317a831c5d5cb12e244745d3f2f70f738
chad <ccrowe@unomaha.edu>
2017-05-25 09:39:58 -0500

    Created cache_line struct for Anu to manipulate.  Blocks then contain cache_lines.

--- Block.c
+++ Block.c
@@ -6,5 +6,11 @@
 
 Block Constructor_Block(){
     Block block = {};
+    block.cacheLines = (struct CacheLineTag*)malloc(sizeof(struct CacheLineTag));
+    block.SetCacheLines = &SetCacheLines;
     return block;
+}
+
+void SetCacheLines(Block block,CacheLine* cacheLines[]){
+    block.cacheLines = cacheLines;
 }
--- Block.h
+++ Block.h
@@ -6,6 +6,7 @@
 #define ACA_PROJECT_BLOCK_H
 #include <stdbool.h>
 #include "uthash/include/uthash.h"//troydhanson.github.io/uthash
+#include "cache_line.h"
 
 typedef struct BlockTag{
     int size;
@@ -13,12 +14,16 @@ typedef struct BlockTag{
     bool validBit;
     bool dirtyBit;
 
-    int useFrequency;
+    int useFrequency;//for LRU in Set
 
+    void (*SetCacheLines)(struct BlockTag* block,CacheLine cacheLines[]);
+    CacheLine* cacheLines;//number in array is the number of cache lines in the block
 
     UT_hash_handle hh; /* make this structure hashable*/
 } Block;//Block is synonymous for struct BlockTag (they mean the same thing).
 
 Block Constructor_Block();//construct to call to obtain a Block Instance
+
+void SetCacheLines(Block block,CacheLine* cacheLines[]);
 
 #endif //ACA_PROJECT_BLOCK_H
