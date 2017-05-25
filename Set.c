d11f3e397fbbd12e4564ec91a50236606e457070
chad <ccrowe@unomaha.edu>
2017-05-25 07:23:49 -0500

    Added Constructor_Set for Anu to use in Set.c

--- Set.c
+++ Set.c
@@ -6,49 +6,52 @@
 #include "Set.h"
 #include <stdio.h>
 
-Block *HashTable = NULL;
 
-void Add_Block(Block *s) { //The first parameter to HASH_ADD_INT  is the hash table.  the second parameter is the NAME of the key field in the object.  This is macro magic
-    HASH_ADD_INT( HashTable, id, s );//add s and id to hashmap
+Set Constructor_Set(){
+    Set set = {};
+    set.put = put;
+    set.Add_Or_Replace = Add_Or_Replace;
+    set.get = get;
+    set.removeFromTable = removeFromTable;
+    set.delete_all = delete_all;
+    set.Count = Count;
+    return set;
+}
+
+void put(Block* HashTable,Block *value) {  //key is tag of the block.  Seems magical
+    HASH_ADD_INT( HashTable, tag, value );//add s and id to hashmap
     //The last parameter is a pointer to the structure being added
 }
 
 
 //HASH_REPLACE  is equivalent to HASH_ADD but it finds and deletes that item first
-void Add_Block_Check_If_Exists(int id, Block* value) {
-    struct BlockTag *s;
+void Add_Or_Replace(Block* HashTable,int key) {
+    struct BlockTag *hashTableStoresInThisBlock;//to store getter
 
-    HASH_FIND_INT(HashTable, &id, s);  /* id already in the hash? */
-    if (s==NULL) {
-        s = (struct BlockTag*)malloc(sizeof(struct BlockTag));
-        s->id = id;
-        HASH_ADD_INT( HashTable, id, s );  /* id: name of key field */
+    HASH_FIND_INT(HashTable, &key, hashTableStoresInThisBlock);  /* id already in the hash? */
+    if (hashTableStoresInThisBlock==NULL) {
+        hashTableStoresInThisBlock = (struct BlockTag*)malloc(sizeof(struct BlockTag));
+        hashTableStoresInThisBlock->tag = key;
+        HASH_ADD_INT( HashTable, tag, hashTableStoresInThisBlock );  /* id: name of key field */
     }
 }
 
 
 //look up item in hashmap
-Block* Get_Block(int block_id) {
-    Block *s;
+Block* get(Block* HashTable,int key) {
+    Block *hashTableStoresInThisBlock;
 
-    HASH_FIND_INT( HashTable, &block_id, s );//find block_id and put into s
-    return s;
+    HASH_FIND_INT( HashTable, &key, hashTableStoresInThisBlock );//find block_id and put into s
+    return hashTableStoresInThisBlock;
 }
 
 //delete from hashmap
-void Delete_Block(Block *blocks) {
-    HASH_DEL( HashTable, blocks);//removes blocks of type block
-}
-
-void InitializeSet(){
-    Set set = {};
-    set.Add_Block = Add_Block;
-    set.Get_Block = Get_Block;
-    set.Delete_Block = Delete_Block;
+void removeFromTable(Block* HashTable,Block *blockToRemove) {
+    HASH_DEL( HashTable, blockToRemove);//removes blocks of type block
 }
 
 //Delete all items from hash
-void delete_all() {
+void delete_all(Block* HashTable) {
     struct BlockTag *current_block, *tmp;
 
     HASH_ITER(hh, HashTable, current_block, tmp) {
@@ -58,7 +61,7 @@ void delete_all() {
 }
 
 //count items
-int Count(){
+int Count(Block* HashTable){
     int num_in_hashtable;
     num_in_hashtable = HASH_COUNT(HashTable);
     return num_in_hashtable;
