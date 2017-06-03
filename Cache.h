3bb93b3a386c13eb067fb31d560300e1cddfa4e9
chad <ccrowe@unomaha.edu>
2017-06-03 11:49:12 -0500

    Updated Cache

--- Cache.h
+++ Cache.h
@@ -4,9 +4,14 @@
 
 #ifndef ACA_PROJECT_CACHE_H
 #define ACA_PROJECT_CACHE_H
+#include "Set.h"
+
 
 typedef struct CacheTag{
     int setAssociativity;
+    Set ** sets;
+    //write-back for L1 & L2
+    //write-allocate for L1 & L2
 } Cache;
 
 Cache Constructor_Cache(int setAssociativity);
