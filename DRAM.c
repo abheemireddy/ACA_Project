5de199224d9bdb0c39743a311077eef1ac109d42
chad <ccrowe@unomaha.edu>
2017-06-05 11:52:41 -0500

    Finish implementing a basic DRAM

--- DRAM.c
+++ DRAM.c
@@ -21,19 +21,7 @@ void putBlock(Block** HashTable,Block *value) {  //key is useFrequency of the bl
         printf("The passed block needs to have attribute address set");
     }
     HASH_ADD_INT(*HashTable, address.bitStringValue,value );
-    //The last parameter is a pointer to the structure being added
 }
-
-/*void replace(UT_hash_handle hh,Block** HashTable,Block *value) {
-    struct BlockTag *hashTableStoresInThisBlock;//to store getter
-
-    HASH_FIND_STR( *HashTable, value->address.bitString, hashTableStoresInThisBlock );
-    if (hashTableStoresInThisBlock==NULL) {
-        hashTableStoresInThisBlock = (struct BlockTag*)malloc(sizeof(struct BlockTag));
-        hashTableStoresInThisBlock->address.bitString = value->address.bitString;
-        HASH_ADD_KEYPTR(hh,*HashTable, value->address.bitString, strlen(value->address.bitString),value );
-    }
-}*/
 
 Block* getBlock(Block** HashTable,int key) {
     Block *hashTableStoresInThisBlock;
@@ -42,7 +30,6 @@ Block* getBlock(Block** HashTable,int key) {
     return hashTableStoresInThisBlock;
 }
 
-//count items
 int CountBlocks(Block** HashTable){
     int num_in_hashtable;
     num_in_hashtable = HASH_COUNT(*HashTable);
@@ -53,6 +40,6 @@ void print_all_blocks(Block** HashTable) {
     Block* s;
     Block* tmp;
     HASH_ITER(hh,*HashTable,s,tmp){
-        printf("address: %s, LFU:%d\n",s->address.bitString,s->useFrequency);
+        printf("address: %d\n",s->address.bitStringValue);
     }
 }
