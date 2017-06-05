fbda28a81644174cf6d1b7d6ac0845075f5e1564
chad <ccrowe@unomaha.edu>
2017-06-05 11:02:25 -0500

    Created BlockTransferer basic struct

---
+++ BlockTransferer.h
@@ -0,0 +1,14 @@
+//
+// Created by chad on 6/5/17.
+//
+
+#ifndef ACA_PROJECT_BLOCKTRANSFERER_H
+#define ACA_PROJECT_BLOCKTRANSFERER_H
+
+//This struct will handle block transfers between memory and the L1/L2 caches.
+typedef struct TransfererTag{
+
+} Transferer;
+
+
+#endif //ACA_PROJECT_BLOCKTRANSFERER_H
