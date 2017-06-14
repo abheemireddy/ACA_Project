e0abc318f5ed84bd8bd9cf04a027d5f3bd2ee4e0
ccrowe@unomaha.edu <ccrowe@unomaha.edu>
2017-06-14 03:49:34 -0500

    Moved directories so it can build on loki

--- cmake_install.cmake
+++ cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/abheemireddy/ACA_Project
+# Install script for directory: /home/ccrowe/ACA_Project
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/abheemireddy/ACA_Project/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/ccrowe/ACA_Project/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
